2fbd9ac5f94132e78c2108b85a64d14b80d32436
Tato <tatorobaqidze@icloud.com>
2023-05-09 00:04:23 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+import LayoutWrapper from "./components/layout/LayoutWrapper";
+
+function App() {
+  return (
+    <LayoutWrapper>
+      {/* hero */}
+      {/* slider */}
+    </LayoutWrapper>
+  );
+}
+
+export default App;
---
+++ src/components/layout/LayoutWrapper.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const LayoutWrapper = ({ children }) => {
+  return (
+    <Container>
+      <Navbar />
+      {children}
+      <Footer />
+    </Container>
+  );
+};
+
+export default LayoutWrapper;
